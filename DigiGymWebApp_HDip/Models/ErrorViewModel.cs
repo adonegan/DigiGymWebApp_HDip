2ba8316e888965ebacf3ae75016867ed90441f7f
adonegan <donegan.aileen@gmail.com>
2024-05-22 20:26:41 +0100

    initial commit

---
+++ DigiGymWebApp_HDip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigiGymWebApp_HDip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
