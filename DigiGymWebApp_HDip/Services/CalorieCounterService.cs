2fbf23a9312927839b3f69f2d621ad27e01d8b5d
adonegan <donegan.aileen@gmail.com>
2024-06-12 20:51:44 +0100

    Accept userId parameter so count can be attributed to logged in user

--- DigiGymWebApp_HDip/Services/CalorieCounterService.cs
+++ DigiGymWebApp_HDip/Services/CalorieCounterService.cs
@@ -8,17 +8,17 @@ namespace DigiGymWebApp_HDip.Services
     public class CalorieCounterService
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
 
         public CalorieCounterService(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        public async Task<int> GetTotalCalories(DateTime date)
+        public async Task<int> GetTotalCalories(DateTime date, string userId)
         {
+            // User not available directly inside method outside controller, so get it in controller and pass via parameter instead
             var foodEntriesByDate = await _context.FoodDiary
-                                        .Where(f => f.CreatedAt.Date == date.Date)
+                                        .Where(f => f.CreatedAt.Date == date.Date && f.Id == userId)
                                         .ToListAsync();
 
             return foodEntriesByDate.Sum(f => f.Calories);
