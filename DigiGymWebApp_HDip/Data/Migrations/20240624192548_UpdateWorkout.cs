daa1806ec40d005c02579beeae146d7fcb6b4644
adonegan <donegan.aileen@gmail.com>
2024-07-27 13:39:45 +0100

    workout data migration updates

---
+++ DigiGymWebApp_HDip/Data/Migrations/20240624192548_UpdateWorkout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigiGymWebApp_HDip.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWorkout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Workouts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherInfo",
+                table: "Workouts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "OtherInfo",
+                table: "Workouts");
+        }
+    }
+}
