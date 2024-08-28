479e9aaef7f86118cce450ea3c176d12f1734630
adonegan <donegan.aileen@gmail.com>
2024-08-28 01:16:54 +0100

    add unit tests for GetBMICategory service

---
+++ AppTests/Services/BMICategoryTests.cs
@@ -0,0 +1,57 @@
+﻿using DigiGymWebApp_HDip.Services;
+
+namespace AppTests.Services
+{
+    [TestClass]
+    public class BMICategoryTests
+    {
+        private BMICategory _bmiCategoryService;
+
+        // setup
+        [TestInitialize]    
+        public void Setup()
+        {
+            _bmiCategoryService = new BMICategory(null); // don't want context here
+        }
+
+        [TestMethod]
+        public async Task Get_Underweight()
+        {
+            // act
+            var result = await _bmiCategoryService.GetBMICategory(17.5);
+
+            // assert
+            Assert.AreEqual("Underweight", result, "Underweight should be anything less than 18.5");
+        }
+
+        [TestMethod]
+        public async Task Get_NormalWeight()
+        {
+            // act
+            var result = await _bmiCategoryService.GetBMICategory(20);
+
+            // assert
+            Assert.AreEqual("Normal", result, "Normal should be between 18.5 and 24.9");
+        }
+
+        [TestMethod]
+        public async Task Get_Overweight()
+        {
+            // act
+            var result = await _bmiCategoryService.GetBMICategory(26);
+
+            // assert
+            Assert.AreEqual("Overweight", result, "Overweight should between 25 and 29.9");
+        }
+
+        [TestMethod]
+        public async Task Get_Obese()
+        {
+            // act
+            var result = await _bmiCategoryService.GetBMICategory(33);
+
+            // assert
+            Assert.AreEqual("Obese", result, "Obese should be anything over 30");
+        }
+    }
+}
